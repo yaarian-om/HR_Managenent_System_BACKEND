38a0f5ecfb4bc0ec10339da246a113b4b897b7d2
“Sudipata <dip.kumar020@gmail.com>
2023-10-03 17:05:08 +0600

    Uploading the Project file with Remote database Configuration

---
+++ src/main/java/com/hr_management_system_backend/HrManagementSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hr_management_system_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrManagementSystemBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrManagementSystemBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hr_management_system_backend/HrManagementSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hr_management_system_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrManagementSystemBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
